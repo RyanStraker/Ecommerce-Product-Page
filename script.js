0a0bd4f8d26511b6c48b44727c610bdc3f2c4e25
Ryan <ryanstraker@proton.me>
2024-07-09 17:43:16 +0100

    All major structural elements now complete. Just need to do styling. Note: position: fixed basket container is very basic and is not optimized for different screen sizes.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
-    
+    //Code to get basket menu to show on click of cart icon//
+
+    var basketMenu = document.getElementById('basket-menu-container');
+    var cartIcon = document.getElementById('basket-icon-clickable');
+
+    cartIcon.addEventListener('click', function () {
+        basketMenu.setAttribute('style', 'display: block');
+    })
 })

0af4bee25d4ca8ca1ebe79c6791d197f529added
Ryan <ryanstraker@proton.me>
2024-07-09 17:09:18 +0100

    Most of major structural work has been completed. Just need to complete the shopping cart in its active state. After that, can move onto precise visual stylings

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
 window.addEventListener('DOMContentLoaded', function() {
-    console.log("Am only testin peh ignore me");
-
-    var clickablePicturesRevealAll = document.querySelectorAll('.col-6 > .row > .col-12 > .row > .col-3');
-    var mainContainer = document.querySelector('.container-fluid');
-    var body = document.getElementsByTagName('body')[0];
-
-    clickablePicturesRevealAll[0].addEventListener('click', function() {
-        console.log("oh my god!");
-        body.style.backgroundColor = "hsl(0, 0%, 0%)";
-        body.style.opacity = 75%;
-    });
-
-    clickablePicturesRevealAll[1].addEventListener('click', function() {
-        console.log("oh my god!");
-    });
-
-    clickablePicturesRevealAll[2].addEventListener('click', function() {
-        console.log("oh my god!");
-    });
-
-    clickablePicturesRevealAll[3].addEventListener('click', function() {
-        console.log("oh my god!");
-    });
-
     
 })

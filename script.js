349b02206ff42f06a4a1ee87ea66a431d5eb6986
Ryan <ryanstraker@proton.me>
2024-07-16 02:02:44 +0100

    Progress made on lightbox. Midway through. To be picked up tomorrow

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
     //Code to get basket menu to show on click of cart icon//
 
+    console.log("loaded pet");
+
     var basketMenu = document.getElementById('basket-menu-container');
     var cartIcon = document.getElementById('basket-icon-clickable');
 
@@ -10,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Code to get opacity to change on click on images underneath main, and to introduce an orange solid border of 2px//
 
-    var mainUnderneath = document.querySelectorAll('div > div > div > a > img');
+    var mainUnderneath = document.getElementsByClassName('non-lightbox-images-underneath');
 
     var mainUnderneaths = document.getElementById('images-underneath-main');
 
@@ -23,6 +25,29 @@ window.addEventListener('DOMContentLoaded', function() {
     mainUnderneathsContainers[0].addEventListener('click', function() {
         mainUnderneathsContainers[0].setAttribute('style', 'border: 2px solid hsl(26, 100%, 55%');
     });
+
+    //Code to get lightbox image to change on clicks//
+
+    var lightboxImagesUnderneath = document.querySelectorAll('div > div > div > a > img');
+
+    lightboxImagesUnderneath[0].addEventListener('click', function () {
+        console.log('clicked pet, theres nothing wrong with lightboxImagesUnderneath');
+    })
+
+    var imageLocations = ['images/image-product-1.jpg', 'images/image-product-2.jpg', 'images/image-product-3.jpg', 'images/image-product-4.jpg'];
+
+    var lightboxImage = document.getElementById('lightbox-main-image');
+
+    var imageChangeClickArea = document.getElementById('images-underneath-row');
+
+    imageChangeClickArea.addEventListener('click', function (event) {
+        console.log("clicked pet");
+        for (var i = 0; i < lightboxImagesUnderneath.length; i++) {
+            if (lightboxImagesUnderneath[i] == event.target) {
+                lightboxImage.setAttribute('src', imageLocations[i]);
+            }
+        }
+    })
 
     //code to get the quantity buying update every time + or - button is clicked//
 

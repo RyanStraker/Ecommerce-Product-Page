d5a169f2e050735b2fe328d0d99c9876ec5e8d7a
Ryan <ryanstraker@proton.me>
2024-07-16 09:10:11 +0100

    Lightbox complete.

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
-    //Code to get basket menu to show on click of cart icon//
+    
 
     console.log("loaded pet");
+
+     //code to get the quantity buying update every time + or - button is clicked//
+
+     var quantityBuying = document.getElementById('amount-buying');
+     var plus = document.getElementById('morePlease');
+     var less = document.getElementById('lessPlease');
+ 
+     var theQuantity = 0;
+ 
+     plus.addEventListener('click', function() {
+         theQuantity++;
+         quantityBuying.innerHTML = theQuantity;
+     });
+ 
+     less.addEventListener('click', function() {
+         if (theQuantity > 0) {
+            theQuantity--;
+            quantityBuying.innerHTML = theQuantity;
+        } else {}
+     });
+
+    //Code to get basket menu to show on click of cart icon//
 
     var basketMenu = document.getElementById('basket-menu-container');
     var cartIcon = document.getElementById('basket-icon-clickable');
@@ -20,19 +42,50 @@ window.addEventListener('DOMContentLoaded', function() {
     
     mainUnderneath[0].addEventListener('click', function() {
         mainUnderneath[0].setAttribute('style', 'opacity: 0.5');
+        mainUnderneath[1].setAttribute('style', 'opacity: 1');
+        mainUnderneath[2].setAttribute('style', 'opacity: 1');
+        mainUnderneath[3].setAttribute('style', 'opacity: 1');
+
     });
 
-    mainUnderneathsContainers[0].addEventListener('click', function() {
-        mainUnderneathsContainers[0].setAttribute('style', 'border: 2px solid hsl(26, 100%, 55%');
+    mainUnderneath[1].addEventListener('click', function() {
+        mainUnderneath[0].setAttribute('style', 'opacity: 1');
+        mainUnderneath[1].setAttribute('style', 'opacity: 0.5');
+        mainUnderneath[2].setAttribute('style', 'opacity: 1');
+        mainUnderneath[3].setAttribute('style', 'opacity: 1');
     });
 
-    //Code to get lightbox image to change on clicks//
+    mainUnderneath[2].addEventListener('click', function() {
+        mainUnderneath[0].setAttribute('style', 'opacity: 1');
+        mainUnderneath[1].setAttribute('style', 'opacity: 1');
+        mainUnderneath[2].setAttribute('style', 'opacity: 0.5');
+        mainUnderneath[3].setAttribute('style', 'opacity: 1');
+    });
 
-    var lightboxImagesUnderneath = document.querySelectorAll('div > div > div > a > img');
+    mainUnderneath[3].addEventListener('click', function() {
+        mainUnderneath[0].setAttribute('style', 'opacity: 1');
+        mainUnderneath[1].setAttribute('style', 'opacity: 1');
+        mainUnderneath[2].setAttribute('style', 'opacity: 1');
+        mainUnderneath[3].setAttribute('style', 'opacity: 0.5');
+    });
 
-    lightboxImagesUnderneath[0].addEventListener('click', function () {
-        console.log('clicked pet, theres nothing wrong with lightboxImagesUnderneath');
-    })
+    //Code to get all opacities to reset to 1 after user clicks close button of modal//
+
+    var closeModalButton = document.getElementById('modal-close');
+
+    closeModalButton.addEventListener('click', function() {
+
+        for (var i = 0; i < mainUnderneath.length; i++) {
+            mainUnderneath[i].setAttribute('style', 'opacity: 1');
+        }
+
+    });
+
+    
+
+    //Code to get lightbox image to change on clicks - WITHIN lightbox//
+
+    var lightboxImagesUnderneath = document.getElementsByClassName('lightbox-thumbnails');
 
     var imageLocations = ['images/image-product-1.jpg', 'images/image-product-2.jpg', 'images/image-product-3.jpg', 'images/image-product-4.jpg'];
 
@@ -41,35 +94,13 @@ window.addEventListener('DOMContentLoaded', function() {
     var imageChangeClickArea = document.getElementById('images-underneath-row');
 
     imageChangeClickArea.addEventListener('click', function (event) {
-        console.log("clicked pet");
         for (var i = 0; i < lightboxImagesUnderneath.length; i++) {
             if (lightboxImagesUnderneath[i] == event.target) {
                 lightboxImage.setAttribute('src', imageLocations[i]);
             }
         }
     })
-
-    //code to get the quantity buying update every time + or - button is clicked//
-
-    var quantityBuying = document.getElementById('amount-buying');
-    var plus = document.getElementById('morePlease');
-    var less = document.getElementById('lessPlease');
-
-    var theQuantity = 0;
-
-    plus.addEventListener('click', function() {
-        theQuantity++;
-        quantityBuying.innerHTML = theQuantity;
-    })
-
-    less.addEventListener('click', function() {
-        theQuantity--;
-        quantityBuying.innerHTML = theQuantity;
-    })
-
-
-
     
-    
+    //code to set lightbox load image as the one which the user has clicked//
 
 })
